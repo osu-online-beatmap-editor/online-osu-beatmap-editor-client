efe51c111ef68256673a44c228ed9633e4688688
DESKTOP-MAN5SL9\Mateusz-PC <2002mateuszbuturla@gmail.com>
2024-04-08 00:42:30 +0200

    Added icons for editor tool bar

--- views/Editor/EditorToolBar.cs
+++ views/Editor/EditorToolBar.cs
@@ -1,8 +1,6 @@
 ﻿using online_osu_beatmap_editor_client.common;
 using online_osu_beatmap_editor_client.components.Button;
 using online_osu_beatmap_editor_client.components.Container;
-using online_osu_beatmap_editor_client.config;
-using SFML.Graphics;
 using System;
 
 namespace online_osu_beatmap_editor_client.views.Editor
@@ -15,17 +13,20 @@ namespace online_osu_beatmap_editor_client.views.Editor
         {
             toolbar = new UIContainer(0, 50, 95, 1080 - 50, 10, ContainerOrientation.Vertical);
 
-            UIButtonIcon button1 = new UIButtonIcon("assets/icons/circle.png");
-            UIButtonIcon button2 = new UIButtonIcon("assets/icons/circle.png");
-            UIButtonIcon button3 = new UIButtonIcon("assets/icons/circle.png");
+            UIButtonIcon buttonSelect = new UIButtonIcon("assets/icons/select.png");
+            UIButtonIcon buttonCircle = new UIButtonIcon("assets/icons/circle.png");
+            UIButtonIcon buttonSlider = new UIButtonIcon("assets/icons/slider.png");
+            UIButtonIcon buttonSpinner = new UIButtonIcon("assets/icons/spinner.png");
 
-            button1.Clicked += (sender, e) => Console.WriteLine("Button 1 clicked!");
-            button2.Clicked += (sender, e) => Console.WriteLine("Button 2 clicked!");
-            button3.Clicked += (sender, e) => Console.WriteLine("Button 3 clicked!");
+            buttonSelect.Clicked += (sender, e) => Console.WriteLine("Button 1 clicked!");
+            buttonCircle.Clicked += (sender, e) => Console.WriteLine("Button 2 clicked!");
+            buttonSlider.Clicked += (sender, e) => Console.WriteLine("Button 3 clicked!");
+            buttonSpinner.Clicked += (sender, e) => Console.WriteLine("Button 3 clicked!");
 
-            toolbar.AddElement(button1);
-            toolbar.AddElement(button2);
-            toolbar.AddElement(button3);
+            toolbar.AddElement(buttonSelect);
+            toolbar.AddElement(buttonCircle);
+            toolbar.AddElement(buttonSlider);
+            toolbar.AddElement(buttonSpinner);
         }
 
         public BaseUIComponent GetComponent()
