1af7e6db82b80f3f592e86e22f6c159e39c9e1ce
DESKTOP-MAN5SL9\Mateusz-PC <2002mateuszbuturla@gmail.com>
2024-04-08 12:54:28 +0200

    Added click handler for editor field

--- views/Editor/EditorField.cs
+++ views/Editor/EditorField.cs
@@ -1,19 +1,27 @@
 ﻿using online_osu_beatmap_editor_client.common;
 using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
+using System;
 
 namespace online_osu_beatmap_editor_client.views.Editor
 {
     public class EditorField : BaseUIComponent
     {
+        protected bool isMouseButtonPressed;
+        protected bool isHovered;
+
+        private float scale = 2.3f;
         private Vector2i baseEditorFielSize = new Vector2i(512, 384); 
         private RectangleShape fieldShape;
         private Color fieldColor = Color.White;
 
         public EditorField(int posX, int posY) : base(posX, posY)
         {
-            this.width = (int)(baseEditorFielSize.X * 2.3f);
-            this.height = (int)(baseEditorFielSize.Y * 2.3f);
+            isMouseButtonPressed = false;
+            isHovered = false; 
+            width = (int)(baseEditorFielSize.X * scale);
+            height = (int)(baseEditorFielSize.Y * scale);
 
             fieldShape = new RectangleShape(new Vector2f(width, height));
             fieldShape.Origin = new Vector2f(width / 2, height / 2); 
@@ -21,14 +29,44 @@ namespace online_osu_beatmap_editor_client.views.Editor
             fieldShape.FillColor = fieldColor;
         }
 
-        public override void Update()
-        {
-            
-        }
-
         public override void Draw()
         {
             window.Draw(fieldShape);
+        }
+
+
+        private void HandleClick(Vector2i clickPoint)
+        {
+            Console.WriteLine(clickPoint);
+        }
+
+        private void AddClickListener()
+        {
+            isHovered = IsMouseOver();
+            if (isHovered && !isMouseButtonPressed && Mouse.IsButtonPressed(Mouse.Button.Left))
+            {
+                isMouseButtonPressed = true;
+                Vector2i mousePosition = Mouse.GetPosition(window);
+                Vector2i clickPoint = new Vector2i(mousePosition.X - posX + width / 2, mousePosition.Y - posY + height / 2);
+                Vector2i unScaledClickPoint = new Vector2i((int)(clickPoint.X / scale), (int)(clickPoint.Y / scale));
+                HandleClick(unScaledClickPoint);
+            }
+            else if (isMouseButtonPressed && !Mouse.IsButtonPressed(Mouse.Button.Left))
+            {
+                isMouseButtonPressed = false;
+            }
+        }
+
+        public override void Update()
+        {
+            AddClickListener();
+        }
+
+        private bool IsMouseOver()
+        {
+            Vector2i mousePosition = Mouse.GetPosition(window);
+            return (mousePosition.X >= posX - width / 2 && mousePosition.X <= posX + width - width / 2 &&
+                    mousePosition.Y >= posY - height / 2 && mousePosition.Y <= posY + height - height / 2);
         }
     }
 }
