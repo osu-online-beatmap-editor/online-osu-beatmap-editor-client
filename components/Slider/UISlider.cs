e826b4ffacae789245888a3fa192f7d60b80c844
DESKTOP-MAN5SL9\Mateusz-PC <2002mateuszbuturla@gmail.com>
2024-04-09 19:54:33 +0200

    Created base slider component

---
+++ components/Slider/UISlider.cs
@@ -0,0 +1,74 @@
+﻿using online_osu_beatmap_editor_client.common;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+
+namespace online_osu_beatmap_editor_client.components.Slider
+{
+    public class UISlider : BaseUIComponent
+    {
+        private RectangleShape track;
+        private RectangleShape knob;
+        private float value = 0.5f;
+
+        public UISlider(Vector2i pos, int width) : base(pos)
+        {
+            size = new Vector2i(width, 20);
+
+            track = new RectangleShape(new Vector2f(size.X, 20));
+            track.FillColor = Color.White;
+            track.Position = new Vector2f(pos.X, pos.Y);
+
+            knob = new RectangleShape(new Vector2f(20, 20));
+            knob.FillColor = Color.Blue;
+            knob.Position = new Vector2f(pos.X + value * size.X, pos.Y);
+        }
+
+        public override void HandleSizeUpdate(Vector2i size)
+        {
+            if (track != null)
+            {
+                track.Size = new Vector2f(size.X, 20);
+            }
+            if (knob != null)
+            {
+                knob.Size = new Vector2f(20, 20);
+            }
+        }
+
+        public override void HandlePositionUpdate(Vector2i pos)
+        {
+            base.HandlePositionUpdate(pos);
+            if (track != null)
+            {
+                track.Position = new Vector2f(pos.X, pos.Y);
+            }
+            if (knob != null)
+            {
+                knob.Position = new Vector2f(pos.X + value * size.X - 10, pos.Y);
+            }
+        }
+
+        public override void Update()
+        {
+            if (Mouse.IsButtonPressed(Mouse.Button.Left))
+            {
+                Vector2i mousePos = Mouse.GetPosition(window);
+                if (mousePos.X >= pos.X && mousePos.X <= pos.X + size.X &&
+                    mousePos.Y >= pos.Y && mousePos.Y <= pos.Y + size.Y)
+                {
+                    value = (mousePos.X - pos.X) / (float)size.X;
+                    knob.Position = new Vector2f(pos.X + value * size.X - 10, pos.Y);
+                }
+            }
+            Console.WriteLine(value);
+        }
+
+        public override void Draw()
+        {
+            window.Draw(track);
+            window.Draw(knob);
+        }
+    }
+}
--- views/Editor/EditorDetailsBar.cs
+++ views/Editor/EditorDetailsBar.cs
@@ -1,7 +1,7 @@
 ﻿using online_osu_beatmap_editor_client.components.Container;
-using online_osu_beatmap_editor_client.components;
 using SFML.System;
 using online_osu_beatmap_editor_client.common;
+using online_osu_beatmap_editor_client.components.Slider;
 
 namespace online_osu_beatmap_editor_client.views.Editor
 {
@@ -12,6 +12,10 @@ namespace online_osu_beatmap_editor_client.views.Editor
         public EditorDetailsBar()
         {
             detailsBar = new UIContainer(new Vector2i(853 * 2 - 200, 40), new Vector2i(200, 480 * 2 - 40), 10, ContainerOrientation.Vertical);
+
+            UISlider slider = new UISlider(new Vector2i(0, 0), 180);
+
+            detailsBar.AddElement(slider);
         }
 
         public BaseUIComponent GetComponent()
