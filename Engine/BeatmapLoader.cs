1f9105f39201c216e9570e37d1db9fc240a68c06
DESKTOP-MAN5SL9\Mateusz-PC <2002mateuszbuturla@gmail.com>
2024-04-11 09:16:49 +0200

    Created base beatmap loader

---
+++ Engine/BeatmapLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace online_osu_beatmap_editor_client.Engine
+{
+    public class BeatmapLoader
+    {
+        private string beatmapFileExtension = ".osu"; 
+
+        public void OpenBeatmapLoadingDialog()
+        {
+            string selectedFile = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = $"Osu beatmap file (*{beatmapFileExtension})|*{beatmapFileExtension}";
+
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                selectedFile = openFileDialog.FileName;
+            }
+
+            LoadBeatmapFromFile(selectedFile);
+        }
+
+        private void LoadBeatmapFromFile(string filePath)
+        {
+            if (filePath == null || !File.Exists(filePath)) {
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);    
+            }
+        }
+    }
+}
