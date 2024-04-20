45ebeb92dabb3702250583636ecf2b1884ac2ef1
DESKTOP-MAN5SL9\Mateusz-PC <2002mateuszbuturla@gmail.com>
2024-04-20 17:42:24 +0200

    Divided window into separate components (navbar, timeline, bottom bar)

---
+++ client.Game/Interfaces/Editor/Components/NavBar.cs
@@ -0,0 +1,36 @@
+﻿using client.Game.Config;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Textures;
+
+namespace client.Game.Interfaces.Editor.Components
+{
+    public partial class NavBar : CompositeDrawable
+    {
+
+        public NavBar() {
+            
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textures)
+        {
+            RelativeSizeAxes = Axes.X;
+            Height = 50;
+            InternalChild = new Container
+            {
+                RelativeSizeAxes = Axes.Both,
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Styles.BACKGROUND_SECONDARY_COLOR,
+                    }
+                }
+            };
+        }
+    }
+}
--- client.Game/Interfaces/Editor/EditorScreen.cs
+++ client.Game/Interfaces/Editor/EditorScreen.cs
@@ -2,13 +2,19 @@ using client.Game.Config;
 using client.Game.Interfaces.Editor.Components;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Screens;
+using osuTK.Graphics;
 
 namespace client.Game.Interfaces.Editor
 {
     public partial class EditorScreen : Screen
     {
+        private Toolbar toolbar;
+
         [BackgroundDependencyLoader]
         private void load()
         {
@@ -19,8 +25,31 @@ namespace client.Game.Interfaces.Editor
                     Colour = Styles.BACKGROUND_COLOR,
                     RelativeSizeAxes = Axes.Both,
                 },
-                new Toolbar {
-                    RelativeSizeAxes = Axes.Y,
+                new Container
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Children = new Drawable[]
+                    {
+                        toolbar = new Toolbar(),
+                        new Container
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Margin = new MarginPadding { Left = Styles.TOOLBAR_GAP * 2 + Styles.TOOLBAR_BUTTON_SIZE },
+                            Children = new Drawable[]
+                            {
+                                new NavBar(),
+                                new Timeline {
+                                    Y = 50, // Height of navbar
+                                },
+                                new Container
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    Margin = new MarginPadding { Top = 50 + 75 }, // Height of navbar and timeline combined
+                                },
+                                new BottomBar(),
+                            }
+                        }
+                    }
                 }
             };
         }
